64ca50e7513dde3c4543ba1b290974fdd246f76b
lepzulnag <gindre.matthieu@gmail.com>
2021-01-19 10:28:40 +0100

    Improve writing speed  by 20% when many Type.Any

--- src/write/toRawData.ts
+++ src/write/toRawData.ts
@@ -16,27 +16,27 @@ export default function toRawData(
 ): ArrayOfBuffers {
 	writeSchema(schema, buffers)
 
-	const dispatch = createDispatcher(schema, new class extends Writer {
-		[Type.Null] = () => {};
-		[Type.Undefined] = () => {};
-		[Type.Any] = (value: any) => toRawData(value, guessSchema(value), buffers);
-		[Type.Boolean] = (value: boolean) => buffers.push(uint8(value ? 1 : 0));
-		[Type.Integer] = (value: number) => buffers.push(int32(value));
-		[Type.PositiveInteger] = (value: number) => buffers.push(uint32(value));
-		[Type.BigInteger] = (value: bigint) => buffers.push(bigInt64(value));
-		[Type.Number] = (value: number) => buffers.push(float64(value));
-		[Type.Date] = (value: Date) => buffers.push(bigInt64(BigInt(value.getTime())));
-		[Type.String] = (value: string) => {
+	const dispatch = createDispatcher(schema, Writer({
+		[Type.Null]: () => {},
+		[Type.Undefined]: () => {},
+		[Type.Any]: (value: any) => toRawData(value, guessSchema(value), buffers),
+		[Type.Boolean]: (value: boolean) => buffers.push(uint8(value ? 1 : 0)),
+		[Type.Integer]: (value: number) => buffers.push(int32(value)),
+		[Type.PositiveInteger]: (value: number) => buffers.push(uint32(value)),
+		[Type.BigInteger]: (value: bigint) => buffers.push(bigInt64(value)),
+		[Type.Number]: (value: number) => buffers.push(float64(value)),
+		[Type.Date]: (value: Date) => buffers.push(bigInt64(BigInt(value.getTime()))),
+		[Type.String]: (value: string) => {
 			buffers.push(encode(value))
 			buffers.push(stopToken)
-		}
-		[Type.RegExp] = (value: RegExp) => {
+		},
+		[Type.RegExp]: (value: RegExp) => {
 			buffers.push(encode(value.source))
 			buffers.push(stopToken)
 			buffers.push(encode(value.flags))
 			buffers.push(stopToken)
 		}
-	})
+	}))
 
 	dispatch(value)
 	return buffers
