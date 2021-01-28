7edb82008fc7564ebbae20c1b6b16bc3f41b31eb
lepzulnag <gindre.matthieu@gmail.com>
2021-01-28 16:39:26 +0100

    Working benchmark

--- benchmark/index.ts
+++ benchmark/index.ts
@@ -5,6 +5,10 @@ import {
 	debunker,
 	debunkerSchema
 } from '../src'
+import {
+	bunkerFile,
+	bunkerFile2,
+} from '../src/node'
 import { formatSize, formatTime } from './formatters'
 import Table from 'cli-table'
 // import zlib from 'zlib'
@@ -17,93 +21,71 @@ import Schema from '../src/Schema'
 import samples from '../samples'
 
 
-const challengers = {
-	'json': (value: any) => Buffer.from(JSON.stringify(value)),
-	'bunker': (value: any) => bunker(value),
-	'bunker (lazy guessing)': (value: any) => bunker(value, 'lazy'),
-	'bunker (with schema)': (value: any, schema: Schema) => bunker(value, schema),
-	'notepack': (value: any) => notepack.encode(value),
-	'msgpack': (value: any) => notepack.encode(value),
-}
-
-const inputs = []
-for (const [sample, value] of Object.entries(samples)) {
-	const name = sample.replace(/\.[^/.]+$/, "")
-	inputs.push([name, [value, guessSchema(value)]])
-}
-
-const results = {}
-for (const [trial, input] of inputs) {
-	for (const [challenger, fn] of Object.entries(challengers)) {
-		const start = performance.now()
-		let operations = 0
-		let time = start
-		do {
-			fn.apply(input)
-			operations++
-			time = performance.now()
-		} while (time < start + 500 && operations < 10)
-		const operationsPerSecond = operations / (time - start) * 1000
-		if (!results[trial]) results[trial] = {}
-		results[trial][challenger] = operationsPerSecond
+async function benchmark() {
+	const challengers = {
+		'json': (value: any) => Buffer.from(JSON.stringify(value)),
+		'bunker': (value: any) => bunker(value),
+		'bunker (with schema)': (value: any, schema: Schema) => bunker(value, schema),
+		'notepack': (value: any) => notepack.encode(value),
+		'msgpack': (value: any) => notepack.encode(value),
 	}
-
-}
-
-
-
-
-// for (const [sample, value] of Object.entries(samples)) {
-// 	const name = sample.replace(/\.[^/.]+$/, "")
-// 	suite.addInput(name, [value, guessSchema(value)])
-// }
-
-/*
-const results = {}
-const challengers = {
-	json: [
-		(value: any) => Buffer.from(JSON.stringify(value)),
-		// (value: Buffer) => JSON.parse(value.toString())
-	],
-	bunker: [ bunker ],
-	notepack: [ notepack.encode ],
-	msgpack: [ msgpack.encode ],
-}
-
-
-// challengers start the fight
-for (const [challenger, [encode]] of Object.entries(challengers)) {
+	
+	const inputs = []
 	for (const [sample, value] of Object.entries(samples)) {
-		// const schema = guessSchema(value)
-		const start = performance.now()
-		const encoded = encode(value, 'lazy')
-		const time = performance.now() - start
-		if (!results[challenger]) results[challenger] = {}
-		results[challenger][sample] = { time, size: encoded.length }
+		const name = sample.replace(/\.[^/.]+$/, "")
+		inputs.push([name, [value, guessSchema(value)]])
 	}
-}
-*/
-
-
-// now let's display results
-const sampleNames = Object.keys(samples).map(sample => sample.replace(/\.[^/.]+$/, ""))
-const timeTable = new Table({
-	head: ['SPEED', ...sampleNames],
-})
-const sizeTable = new Table({
-	head: ['SIZE', ...sampleNames],
-})
-for (const challenger in results) {
-	const timeRow = [challenger]
-	const sizeRow = [challenger]
-	for (const sample in results[challenger]) {
-		const { time, size } = results[challenger][sample]
-		timeRow.push(formatTime(time))
-		sizeRow.push(formatSize(size))
+	
+	const results = {}
+	for (const [trial, input] of inputs) {
+		let fastestChallenger: string
+		let fastestResult = -1
+	
+		for (const [challenger, fn] of Object.entries(challengers)) {
+			let operations = 0
+			let start = performance.now()
+			let time: number
+	
+			do {
+				await fn.apply(null, input)
+				operations++
+				time = performance.now()
+			} while (operations < 500)
+			
+			const operationsPerSecond = operations / (time - start) * 1000
+			
+			if (!results[trial]) results[trial] = {}
+			results[trial][challenger] = operationsPerSecond
+			
+			if (operationsPerSecond > fastestResult) {
+				fastestChallenger = challenger
+				fastestResult = operationsPerSecond
+			}
+		}
+		results[trial].fastest = fastestChallenger
 	}
-	timeTable.push(timeRow)
-	sizeTable.push(sizeRow)
+	
+	
+	
+	// now let's display results
+	const timeTable = new Table({
+		head: ['SPEED', ...Object.keys(challengers)],
+	})
+	// const sizeTable = new Table({
+	// 	head: ['SIZE', ...sampleNames],
+	// })
+	for (const trial in results) {
+		const timeRow = [trial.replace(/\.[^/.]+$/, "")]
+		for (const challenger in results[trial]) {
+			if (challenger == 'fastest') continue
+			const speed = results[trial][challenger]
+			timeRow.push(~~speed + ' ops/s')
+		}
+		timeTable.push(timeRow)
+	}
+	
+	console.log(timeTable.toString())
+	// console.log(sizeTable.toString())
 }
 
-console.log(timeTable.toString())
-console.log(sizeTable.toString())
+benchmark()
