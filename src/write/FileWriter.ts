61124445f0cf132e6da74171264a36464e9ccae7
lepzulnag <gindre.matthieu@gmail.com>
2021-01-28 15:46:00 +0100

    Memory-efficient file writing

--- src/write/FileWriter.ts
+++ src/write/FileWriter.ts
@@ -1,38 +1,31 @@
-import { FileHandle, open } from "fs/promises"
+import { open, writeSync, close } from "fs"
 import BufferWriter from "./BufferWriter"
 import guessSchema from '../guessSchema'
 import Type from '../Type'
-
-type Chunk = [ buffer: Uint8Array, size: number ]
-
 
 /**
  * File writing use a double-buffering implementation.
  */
 export default class FileWriter extends BufferWriter {
-	protected chunksToWrite: Chunk[] = []
-	protected writeQueue = []
-	protected file?: FileHandle
-	protected openingFile: Promise<FileHandle>
-	protected readingDone = false
-	protected end?: (value: boolean) => void
+	file!: number
 
-	constructor(filename: string) {
-		super(8192)
-		this.openingFile = open(filename, 'w')
+	constructor(readonly filename: string) {
+		super()
 	}
 
 	/**
 	 * Start the writing with the given value and schema
 	 */
-	async write(value: any, schema = guessSchema(value)) {
-		const result = new Promise(resolve => this.end = resolve)
-		this[Type.Any](value, schema)
-		// we push the last chunk and we indicate reading is done
-		this.chunksToWrite.push([this.buffer, this.size])
-		this.readingDone = true
-		this.writeFile()
-		return await result
+	write(value: any, schema = guessSchema(value)) {
+		return new Promise<void>((resolve, reject) => {
+			open(this.filename, 'w', (error, file) => {
+				if (error) return reject(error)
+				this.file = file
+				this[Type.Any](value, schema)
+				this.writeFile()
+				close(file, error => error ? reject(error) : resolve())
+			})
+		})
 	}
 
 	/**
@@ -40,31 +33,17 @@ export default class FileWriter extends BufferWriter {
 	 * Instead, the old buffer is set to write to the output file.
 	 * And a new
 	 */
-	protected resizeBuffer() {
-		this.chunksToWrite.push([this.buffer, this.size])
-		this.buffer = new Uint8Array(this.capacity)
-		this.view = new DataView(this.buffer.buffer)
-		this.size = 0
-		if (this.chunksToWrite.length == 1) this.writeFile()
+	protected resizeBuffer(newSize: number) {
+		if (this.capacity == 64)
+			super.resizeBuffer(newSize)
+		else this.writeFile()
 	}
 
 	/**
-	 * Write the current chunk to the file
+	 * Write the current buffer to the file
 	 */
-	protected async writeFile() {
-		if (!this.file) this.file = await this.openingFile
-		
-		if (this.chunksToWrite.length == 0) {
-			if (this.readingDone) {
-				await this.file.close()
-				this.file = undefined
-				this.end!(true)
-			}
-		}
-		else {
-			const [buffer, size] = this.chunksToWrite.shift() as Chunk
-			await this.file.write(buffer, 0, size)
-			this.writeFile()  // we write the next chunk
-		}
+	protected writeFile() {
+		writeSync(this.file, this.buffer, 0, this.size)
+		this.size = 0
 	}
 }
